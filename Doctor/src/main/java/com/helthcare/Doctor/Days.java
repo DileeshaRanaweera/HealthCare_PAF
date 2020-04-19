79f5aea50b42e51c22ed6958a1eecf578530076c
yasasmi <dyasasmiliyanage@gmail.com>
2020-04-19 14:29:04 +0530

    CRUD oparations of Doctor

---
+++ Doctor/src/main/java/com/helthcare/Doctor/Days.java
@@ -0,0 +1,27 @@
+package com.helthcare.Doctor;
+
+public class Days {
+	
+	//table doctor_days data
+		private String DaysOfvisiting;
+		private String NIC;
+	
+	
+	
+////getters & setters doctor_days
+		
+	public String getNIC() {
+			return NIC;
+	}
+	public void setNIC(String nIC) {
+			NIC = nIC;
+	}
+	
+	public String getDaysOfvisiting() {
+		return DaysOfvisiting;
+	}
+	public void setDaysOfvisiting(String daysOfvisiting) {
+		DaysOfvisiting = daysOfvisiting;
+	}
+
+}
---
+++ Doctor/src/main/java/com/helthcare/Doctor/Fee.java
@@ -0,0 +1,32 @@
+package com.helthcare.Doctor;
+
+public class Fee {
+	
+	
+	private String NIC;
+	private String feeid;
+	private String feeamount;
+	public String getNIC() {
+		return NIC;
+	}
+	public void setNIC(String nIC) {
+		NIC = nIC;
+	}
+	public String getFeeid() {
+		return feeid;
+	}
+	public void setFeeid(String feeid) {
+		this.feeid = feeid;
+	}
+	public String getFeeamount() {
+		return feeamount;
+	}
+	public void setFeeamount(String feeamount) {
+		this.feeamount = feeamount;
+	}
+	
+	
+	
+	
+
+}
